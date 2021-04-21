47824dbb22f43f988b139c41db53f4a9897bdfa2
huangnuozhong <171628543@qq.com>
2021-04-21 21:13:08 +0800

    商品检索服务 finished

--- mymall-third-party/src/test/java/com/hnz/mymall/thirdparty/MymallThirdPartyApplicationTests.java
+++ mymall-third-party/src/test/java/com/hnz/mymall/thirdparty/MymallThirdPartyApplicationTests.java
@@ -20,8 +20,8 @@ class MymallThirdPartyApplicationTests {
         // // Endpoint以杭州为例，其它Region请按实际情况填写。
         // String endpoint = "http://oss-cn-shenzhen.aliyuncs.com";
         // // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        // String accessKeyId = "LTAI4G9FYDjpSWetZoAELrtU";
-        // String accessKeySecret = "MboRrDW02oHzGgcwVL9eFbyancvRcZ";
+        // String accessKeyId = "";
+        // String accessKeySecret = "";
         //
         // // 创建OSSClient实例。
         // OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
