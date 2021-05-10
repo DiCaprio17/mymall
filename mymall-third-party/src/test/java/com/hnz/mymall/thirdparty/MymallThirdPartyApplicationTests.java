9fb265b4c46414bafab3eba84806f1f933964d12
huangnuozhong <171628543@qq.com>
2021-05-10 20:00:00 +0800

    RabbitMQ消息队列

--- mymall-third-party/src/test/java/com/hnz/mymall/thirdparty/MymallThirdPartyApplicationTests.java
+++ mymall-third-party/src/test/java/com/hnz/mymall/thirdparty/MymallThirdPartyApplicationTests.java
@@ -31,7 +31,7 @@ class MymallThirdPartyApplicationTests {
         // String endpoint = "http://oss-cn-shenzhen.aliyuncs.com";
         // // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
         // String accessKeyId = "";
-        // String accessKeySecret = "MboRrDW02oHzGgcwVL9eFbyancvRcZ";
+        // String accessKeySecret = "";
         //
         // // 创建OSSClient实例。
         // OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
@@ -137,6 +137,6 @@ class MymallThirdPartyApplicationTests {
 
     @Test
     public void sendSmsCode() {
-        smsComponent.sendCode("15580220501", "**code**:12345,**minute**:5");
+        smsComponent.sendCode("18506823192", "**code**:12345,**minute**:5");
     }
 }
